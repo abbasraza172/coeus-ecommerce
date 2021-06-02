297abe5287a5c7dada8f5e281206e53b706e433b
arslanyousaf77 <arslanyousaf77@gmail.com>
2021-06-02 17:34:12 +0500

    done required changes in PR

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,4 @@
 class Feedback < ApplicationRecord
+    # belongs to association
     belongs_to :reviewable, polymorphic: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,13 @@
 class Product < ApplicationRecord
+    # belongs to association
     belongs_to :user
-    validates :name, presence: true
 
+    # has many associations 
     has_many :pictures, as: :imageable
-
     has_many :categories, through: :product_categories
-
     has_many :orders, through: :order_products
-
     has_many :feedbacks, as: :reviewable
+
+    #validation
+    validates :name, presence: true
 end
