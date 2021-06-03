7a36f9f9c41ac6429b5537cbfc05883802b4ae48
arslanyousaf77 <arslanyousaf77@gmail.com>
2021-06-03 19:47:27 +0500

    single product dynamic view done without related products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,20 @@ class HomeController < ApplicationController
     layout 'default_layout'
     
     def index
+        @categories = Category.where(status: true).order(name: "ASC")
+        @new_products = Product.order(created_at: "DESC").limit(6)
+        @popular_products = Product.all
+        @featured_products = Product.all
+    end
+
+    def show
+        @categories = Category.where(status: true).order(name: "ASC")
+        @product = Product.find_by(id: params[:id])
+        if @product.nil? || @product.status == false
+           flash.now[:alert] = "Product was not found"
+           redirect_to index_path
+        end
+        # @related_products = 
     end
 
     def main
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :products, through: :order_products
-  enum status: { pending: 0, accepted: 1, failed: 2, processing: 3, canceled: 4, delivered: 5 }
+  enum status: { pending: 0, accepted: 1, rejected: 2, processing: 3, canceled: 4, delivered: 5 }
 end
